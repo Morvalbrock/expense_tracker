ad2def71eb677eb46bc150b9a548f5dc9154e2b7
Morvalbrock <agilansr222@gmail.com>
2023-10-26 07:40:40 +0530

    file alignment and expenses_list list a all data

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
@@ -7,7 +8,9 @@ class ExpensesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemBuilder: (context, index) => Text(expenses[index].title),
+      itemBuilder: (context, index) => ExpensesItem(
+        expenses[index],
+      ),
       itemCount: expenses.length,
     );
   }
